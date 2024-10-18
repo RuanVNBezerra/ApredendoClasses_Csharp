9932a0da5fe97fd448dbf4b72501cb679a5bd8b3
RuanVNBezerra <164013416+RuanVNBezerra@users.noreply.github.com>
2024-10-18 17:39:26 -0300

    Adicionar arquivos de projeto.

---
+++ ApredendoC#/Carro.cs
@@ -0,0 +1,19 @@
+﻿namespace ApredendoClasses
+{
+    // clase chamada carro
+     class Carro
+    {
+        // função para ligar o carro
+        // fazendo a função se torna publica para ser usada em outros lugares fora da clase Carro.
+       public void Ligar()
+        {
+            Console.WriteLine("Carro ligado! vrummmm");
+        }
+        // função para desligar o carro
+        // tornando publico aqui tambem.
+        public void Desligar()
+        {
+            Console.WriteLine("Carro desligado!");
+        }
+    }
+}
---
+++ ApredendoC#/Program.cs
@@ -0,0 +1,15 @@
+﻿
+namespace ApredendoClasses;
+internal class Program
+{
+    public static void Main()
+    {
+        // o "meuCarro" se transforma em um objeto do tipo Carro e o "new Carro()" faz o objeto se torna uma instâcia concreta da clase carro
+        Carro meuCarro = new Carro();
+        // chamando a função "Ligar", irá exibir a mensagem que está dentro dela
+        meuCarro.Ligar();
+        // aqui acontece a mesma coisa
+        meuCarro.Desligar();
+    }
+}
+
